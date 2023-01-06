8b14014beb0b894be34377f726dd5bb8c72518bb
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-06 16:35:26 +0000

    moving to css modules

--- src/components/UI/card.js
+++ src/components/UI/card.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import "./card.module.css";
+import classes from "./card.module.css";
 
 const Card = (props) => {
-  const classes = "card " + props.className;
-
-  return <div className={classes}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 };
 
 export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../UI/card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -8,7 +9,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
