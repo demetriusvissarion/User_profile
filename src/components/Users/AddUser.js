85d167cb8eb47d6cea21d4c7a416e9b04ceb3e2c
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-06 16:13:01 +0000

    adding card module

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Card from "../UI/card";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
