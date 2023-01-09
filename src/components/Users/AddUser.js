abcd82a595e2afd4e3cc30ee83244f2947e55e7d
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-09 11:10:53 +0000

    added validation and reseting

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/card";
 import Button from "../UI/button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [users, setUsers] = useState([]);
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    const newUser = {
+      username: enteredUsername,
+      age: enteredAge,
+      id: Math.random().toString(),
+      key: Math.random().toString(),
+    };
+
+    setUsers((prevUsers) => {
+      return [...prevUsers, newUser];
+    });
+    console.log(users);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
